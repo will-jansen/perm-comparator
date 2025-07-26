c3bb5784c4b887ea9c786901b7c051b620419074
will-jansen <will@willjansen.com.au>
2025-07-26 22:04:18 +1000

    update indexController and compare

--- src/main/java/com/permcomparator/controller/IndexController.java
+++ src/main/java/com/permcomparator/controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "forward:/index.html";
     }
+    
+    @GetMapping("/compare")
+    public String compare() {
+        return "forward:/compare.html";
+    }
 }
