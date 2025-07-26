22fa429640e4d49cd48d545ec9c509c71e412941
will-jansen <will@willjansen.com.au>
2025-07-26 21:08:50 +1000

    update to app.java
    cache.delete removed

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -157,10 +157,6 @@ public class Application extends Controller {
 	public static void sforceLogout() {
 		String instanceURL = null;
 		
-		// remove user perms from cache on logout
-		Cache.delete(session.getId() + "-oauth");
-		Cache.delete(session.getId() + "-userperms");
-		
 		if (ForceDotComOAuth2.isLoggedIn()) {
 			instanceURL = ForceDotComOAuth2.getOAuthSession().instance_url;
 			ForceDotComOAuth2.logout();
